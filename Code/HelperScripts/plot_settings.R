451f511e6563c0810262f8aac026ff39d3d8943e
Michael Sieler <sielerjm@oregonstate.edu>
2022-02-25 13:51:29 -0800

    add files

--- Code/HelperScripts/plot_settings.R
+++ Code/HelperScripts/plot_settings.R
@@ -1,28 +1,34 @@
 # Plot Settings
 
-
 #   COLOR PALETTE
 #     RcolorBrewer: https://www.datanovia.com/en/blog/the-a-z-of-rcolorbrewer-palette/
 pal.bupu <- brewer.pal(9, "BuPu")  # specifies the length of the color palette and the name from RcolorBrewer
 pal.dark2 <- brewer.pal(8, "Dark2")
 pal.brbg <- brewer.pal(11, "BrBG")
 pal.RdYlGn <- brewer.pal(11, "RdYlGn")
+pal.PuOr <- brewer.pal(9, "PuOr") 
+pal.YlGn <- brewer.pal(9, "YlGn")
+pal.PuRd <- brewer.pal(9, "PuRd")
+pal.RdPu <- brewer.pal(9, "RdPu")
 pal.grey <- brewer.pal(9, "Greys")
 pal.paired <- brewer.pal(12, "Paired")
-pal.spectrum <- brewer.pal(9, "Spectral")
+pal.spectrum <- brewer.pal(11, "Spectral")
+pal.accent <- brewer.pal(11, "Accent")
+pal.set1 <- brewer.pal(9, "Set1")
 
 
 #   Color codes for variables  
 #     Might need changing depending how the figures look
 
-# col.timepoint <- c(pal.dark2[4], pal.dark2[8])  # creates a range of colors between those indexes
-# col.treatment <- c(pal.dark2[8], pal.dark2[6], pal.dark2[7])  # creates a list of colors
-# col.sample <- pal.dark2[1:3]
-# col.sex <- pal.dark2[1:2]
-# # col.severity <- c(pal.bupu[3], pal.bupu[5], pal.bupu[7], pal.bupu[9])
-# col.exposed <- c(pal.paired[1:2])
-# col.infected <- c(pal.paired[3:4])
-# col.spectrum <- c(pal.spectrum[1:9])
+col.diet <- pal.dark2[1:3]
+col.timepoint <- c(pal.dark2[4], pal.dark2[8])  # creates a range of colors between those indexes
+col.treatment <- c(pal.dark2[8], pal.dark2[6], pal.dark2[7])  # creates a list of colors
+col.sex <- pal.dark2[1:2]
+col.severity <- c(pal.YlGn[4], pal.YlGn[6], pal.YlGn[7], pal.YlGn[9])
+col.exposed <- c(pal.paired[6], pal.paired[2])
+col.infected <- c(pal.dark2[5:6])
+col.spectrum <- c(pal.spectrum[1:9])
+col.diverge <- c(pal.PuOr[1:9])
 
 
 ################################################################################
@@ -31,11 +37,11 @@ pal.spectrum <- brewer.pal(9, "Spectral")
 
 #   Cowplot settings 
 #     Standardizing ggplot settings
-#     (adapted from Keaton Stagaman)
+#     (adapted and modified from Keaton Stagaman)
 my_theme <- theme_update(
-  legend.position = "bottom",
+  legend.position = "none",
   legend.box = "vertical",
-  legend.box.just = "left",
+  legend.box.just = "center",
   legend.title = element_text(size = 10),
   legend.text = element_text(size = 11),
   legend.key = element_rect(fill = "white"),
@@ -58,3 +64,8 @@ my_theme <- theme_update(
 
 ################################################################################
 ################################################################################
+
+# brewer_opts <- {
+#   scale_fill_brewer(palette = "Dark2") +
+#   scale_color_brewer(palette = "Dark2")
+# }
